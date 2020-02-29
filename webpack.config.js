5597d1dc5172bdf29a15c32ddb160d915f25b4f1
husky_shiqi <husky_shiqi@outlook.com>
2020-02-29 21:22:19 +0800

    配置html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('htmp-webpack-plugin');
+
+
+//  创建一个插件的实例
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: path.join(__dirname, './src/index')
+})
+
+
+
 //  向外暴露一个打包的配置对象
 //  因为webpack是基于Node.JS开发的所以支持所有Node.js的语法
 
