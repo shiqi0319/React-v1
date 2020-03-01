00f9fb830e510e4657ee3f9d8fba8850b5d043f7
shi qi <husky_shiqi@outlook.com>
2020-03-01 22:11:36 +0800

    安装babel,初学react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('ok');
+//  这两个导入的时候成员名称必须写
+
+import React from 'react';  //  创建组件 虚拟DOM 生命周期
+import ReactDOM from 'react-dom';   //  把创建好的组件和虚拟DOM放到页面中
+
+//  创建虚拟DOM
+
+// const myh1 = React.createElement('h1', { title: '啊,五环', id: 'myh1'}, '这是一个h1')
+
+
+// const div1 = React.createElement('div', null, '这是一个div', myh1);
+//  使用react-dom把虚拟DOM渲染到页面中
+
+
+//  HTML 是最优秀的标记语言
+//  注意: 在js文件名中,默认不能写这种类似 HTML 的标记;否则打包会失败
+//  可以使用babel来转换这些 js 中的标签
+//  在JS 中,混合写入类似 HTML 的语法,叫做JSX语法,符合XML的JS
+const myDiv = <div id="myDiv" tiile="这是一个div">这是一个div</div>
+
+
+ReactDOM.render(myDiv, document.getElementById('app'));
+
+//  Target container is not a DOM element 第二个参数应该是一个DOM元素
+
+
+//  vue 的写法
+// const vm = new Vue({
+//     data: {},
+//     el: '#app',
+//     methods: {}  //  迈森克斯
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('htmp-webpack-plugin');
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 
 //  创建一个插件的实例
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.join(__dirname, './src/index')
+    template: path.join(__dirname, './src/index.html'),      //  源文件
+    filename: 'index.html'  //  生成的内存中的首页
 })
 
 
 
 //  向外暴露一个打包的配置对象
 //  因为webpack是基于Node.JS开发的所以支持所有Node.js的语法
-
+//  webpack 默认值打包处理 .js后缀名类型的文件, 像.png .vue无法主动处理,所以要配置第三方的loader
 module.exports = {
     mode: 'development',   //  development  production 两个环境
     //  在webpack4.x中有很大的特性，默认的打包文件是src下的 index.js
+    plugins: [
+        htmlPlugin
+    ],
+    module: {   //  所有第三方 模块的配置规则
+        rules: [    //  第三方匹配规则
+            { test: /\.js|jsx$/, use: 'babel-loader', exclude: /node_modules/ } //  千万不要忘记添加 exclude 排除项
+        ]
+    }
 }
